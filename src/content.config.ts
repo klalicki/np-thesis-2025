54ecdcdaa9a3742e5e0c4d5fe770ffbaad7e6d32
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2025-04-09 15:18:42 -0400

    sets up separate markdown files for project and bio

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,12 +3,27 @@ import { glob } from "astro/loaders";
 
 const peopleCollection = defineCollection({
   loader: glob({
-    pattern: "**/*.md",
+    pattern: "*/bio.md",
     base: "./src/content/people",
+    generateId: (options) => {
+      return options.entry.split("/")[0];
+    },
+  }),
+  schema: z.object({}),
+});
+const projectCollection2 = defineCollection({
+  loader: glob({
+    pattern: "*/project.md",
+    base: "./src/content/people",
+    generateId: (options) => {
+      return "bla";
+      return options.entry.split("/")[0];
+    },
   }),
   schema: z.object({}),
 });
 
 export const collections = {
   people: peopleCollection,
+  projects: projectCollection2,
 };
---
+++ src/content/people/sarah-himme/bio.md
@@ -0,0 +1,9 @@
+---
+name: Sarah Himme
+pronouns: she/they
+location: Cobleskill, NY
+headshot: sarah-himme.webp
+portfolio: https://sarahhimme.com
+---
+
+This is Sarah's bio. They are a student from Cobleskill, NY.
